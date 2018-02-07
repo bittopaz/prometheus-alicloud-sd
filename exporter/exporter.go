d26a6cf14affa8a8be8eee070183ec440aad0a5a
haokang.ke <haokang.ke@owtiho.com>
2018-02-07 10:35:10 +0800

    update

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -1,9 +1,12 @@
 package exporter
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"os/exec"
 
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk"
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/ecs"
 )
 
@@ -22,19 +25,90 @@ type Lable struct {
 	Tier    string `json:"tier"`
 }
 
+type Instance struct {
+	REGIONID  string
+	ACCESSKEY string
+	SECRETKEY string
+	TOKEN     string
+}
+
 func EcsClient() (client *ecs.Client) {
-	REGIONID := os.Getenv("ALICLOUD_DEFAULT_REGION")
-	ACCESSKEY := os.Getenv("ALICLOUD_ACCESS_KEY")
-	SECRETKEY := os.Getenv("ALICLOUD_SECRET_KEY")
-	ecsClient, err := ecs.NewClientWithAccessKey(
-		REGIONID,
-		ACCESSKEY,
-		SECRETKEY,
-	)
-	if err != nil {
-		panic(err)
+	var i Instance
+	var err error
+	if os.Getenv("ALICLOUD_DEFAULT_REGION") == "" {
+		path, err := exec.LookPath("curl")
+		if err != nil {
+			panic(err)
+		}
+		//curl path
+
+		// //get instance-id
+		// cmdGetInstanceID := exec.Command(path,
+		// 	"http://100.100.100.200/latest/meta-data/instance-id")
+		// instanceIDRaw, err := cmdGetInstanceID.Output()
+		// if err != nil {
+		// 	panic(err)
+		// }
+		// ID := string(instanceIDRaw)
+		// cmdGetInstanceID.Run()
+
+		//get rolename
+		cmdGetRoleName := exec.Command(path,
+			"http://100.100.100.200/latest/meta-data/ram/security-credentials/")
+		roleNameRaw, err := cmdGetRoleName.Output()
+		if err != nil {
+			panic(err)
+		}
+		ROLENAME := string(roleNameRaw)
+		cmdGetRoleName.Run()
+
+		//according to the rolename, get a json file.
+		cmdGetJSON := exec.Command(path,
+			"http://100.100.100.200/latest/meta-data/ram/security-credentials/"+ROLENAME)
+		jsonRaw, err := cmdGetJSON.Output()
+		if err != nil {
+			panic(err)
+		}
+		cmdGetJSON.Run()
+
+		//convert json file to map
+		var roleMap map[string]*json.RawMessage
+		json.Unmarshal(jsonRaw, &roleMap)
+
+		//extract related content from map
+		json.Unmarshal(*roleMap["AccessKeyId"], &i.ACCESSKEY)
+		json.Unmarshal(*roleMap["AccessKeySecret"], &i.SECRETKEY)
+		json.Unmarshal(*roleMap["SecurityToken"], &i.TOKEN)
+
+		//get instance name/environment/service/tier
+		i.REGIONID = "cn-shanghai"
+		ecsClient, err := sdk.NewClientWithStsToken(
+			i.REGIONID,
+			i.ACCESSKEY,
+			i.SECRETKEY,
+			i.TOKEN,
+		)
+		client = &ecs.Client{
+			Client: *ecsClient,
+		}
+
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		REGIONID := os.Getenv("ALICLOUD_DEFAULT_REGION")
+		ACCESSKEY := os.Getenv("ALICLOUD_ACCESS_KEY")
+		SECRETKEY := os.Getenv("ALICLOUD_SECRET_KEY")
+		client, err = ecs.NewClientWithAccessKey(
+			REGIONID,
+			ACCESSKEY,
+			SECRETKEY,
+		)
+		if err != nil {
+			panic(err)
+		}
 	}
-	return ecsClient
+	return client
 }
 
 func GetInstancesTotalCount(exportertype string) (totalcount int) {
--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/owitho/prometheus-alicloud-sd/exporter"
 	"os"
+
+	"github.com/leo1971/prometheus-alicloud-sd/exporter"
 )
 
 func main() {
